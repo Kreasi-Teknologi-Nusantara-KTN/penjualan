27b32e9e5c7e98a4f0b0a4bf10b06652aed72eec
febriananur <febriananurokhman@gmail.com>
2022-05-28 22:33:17 +0700

    Update db_kios.sql

--- db_kios.sql
+++ db_kios.sql
@@ -1,82 +1,78 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2016 at 09:19 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Waktu pembuatan: 28 Bulan Mei 2022 pada 17.29
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 5.6.40
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `malasngoding_kios`
+-- Database: `db_kios`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
   `uname` varchar(30) NOT NULL,
   `pass` varchar(70) NOT NULL,
-  `foto` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+  `foto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`id`, `uname`, `pass`, `foto`) VALUES
-(8, 'malasngoding', '069c546d1d97fd9648d8142b3e0fd3a3', 'text.png');
+(8, 'kiosktn', 'e96d741d1397bc933dce600f95b53ab5', 'admin.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang`
+-- Struktur dari tabel `barang`
 --
 
-CREATE TABLE IF NOT EXISTS `barang` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
   `nama` text NOT NULL,
   `jenis` text NOT NULL,
   `suplier` text NOT NULL,
   `modal` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `sisa` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+  `sisa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `barang`
+-- Dumping data untuk tabel `barang`
 --
 
 INSERT INTO `barang` (`id`, `nama`, `jenis`, `suplier`, `modal`, `harga`, `jumlah`, `sisa`) VALUES
-(14, 'roti unibis', 'makanan', 'pt.makamur jaya', 5000, 6500, 350, 20),
+(14, 'roti unibis', 'makanan', 'pt.makamur jaya', 5000, 6500, 353, 20),
 (17, 'tim tam', 'makanan ringan', 'pt surga', 2000, 6000, 792, 10),
-(19, 'tic tac', 'makanan ringan', 'pt sido multp', 2000, 4000, 2, 24),
 (20, 'aqua sedang', 'minuman ringan', 'pt jamaika', 1000, 3000, 990, 1000),
-(21, 'makkkanan', 'makanan ringan', 'pt surya', 2000, 4000, 894, 900),
 (22, 'makkkanan', 'makanan ringan', 'pt surya', 2000, 4000, 894, 900),
 (23, 'magnum', 'rokok', 'pt surga', 12000, 13000, 997, 1000),
 (24, 'santri mie', 'makanan ringan', 'pt sido mulyo', 2000, 4000, 784, 800),
 (25, 'rambut palsu', 'accesories', 'pt ptan', 3000, 5000, 496, 500),
 (26, 'rambut palsu', 'accesories', 'pt ptan', 3000, 5000, 496, 500),
 (27, 'sea foog', 'makanan ringan', 'pt surga', 4000, 60000, 598, 600),
-(28, 'mild', 'rokok', 'pt sampoerna', 15000, 17000, 192, 200),
-(29, 'dji sam soe', 'rokok', 'pt ptan', 14000, 15000, 145, 150),
 (30, 'nu mild', 'rokok', 'pt ptan', 14000, 15000, 144, 150),
 (31, 'nu mild', 'rokok', 'pt ptan', 14000, 15000, 144, 150),
 (32, 'roti', 'makanan', 'hdhnhnhh', 2000, 5000, 4, 6);
@@ -84,26 +80,24 @@ INSERT INTO `barang` (`id`, `nama`, `jenis`, `suplier`, `modal`, `harga`, `jumla
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang_laku`
+-- Struktur dari tabel `barang_laku`
 --
 
-CREATE TABLE IF NOT EXISTS `barang_laku` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `barang_laku` (
+  `id` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `nama` text NOT NULL,
   `jumlah` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   `total_harga` int(20) NOT NULL,
-  `laba` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
+  `laba` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `barang_laku`
+-- Dumping data untuk tabel `barang_laku`
 --
 
 INSERT INTO `barang_laku` (`id`, `tanggal`, `nama`, `jumlah`, `harga`, `total_harga`, `laba`) VALUES
-(46, '2015-02-01', 'roti unibis', 2, 6000, 12000, 2000),
 (47, '2015-02-02', 'makkkanan', 7, 12000, 84000, 70000),
 (48, '2015-02-02', 'dji sam soe', 2, 15000, 30000, 2000),
 (49, '2015-02-03', 'makkkanan', 1, 12000, 12000, 10000),
@@ -114,7 +108,6 @@ INSERT INTO `barang_laku` (`id`, `tanggal`, `nama`, `jumlah`, `harga`, `total_ha
 (54, '2015-02-15', 'nu mild', 2, 19100, 38200, 10200),
 (55, '2015-02-27', 'roti unibis', 2, 8000, 16000, 6000),
 (56, '2015-02-19', 'roti unibis', 1, 7000, 7000, 2000),
-(57, '2015-01-14', 'roti unibis', 1, 7000, 7000, 2000),
 (58, '2015-02-01', 'pulpen', 1, 3000, 3000, 2000),
 (59, '2015-02-02', 'roti', 2, 3000, 6000, 2000),
 (63, '2016-01-22', 'tic tac', 8, 4000, 32000, 16000);
@@ -122,24 +115,80 @@ INSERT INTO `barang_laku` (`id`, `tanggal`, `nama`, `jumlah`, `harga`, `total_ha
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengeluaran`
+-- Struktur dari tabel `pengeluaran`
 --
 
-CREATE TABLE IF NOT EXISTS `pengeluaran` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pengeluaran` (
+  `id` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `keperluan` text NOT NULL,
   `nama` text NOT NULL,
-  `jumlah` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `pengeluaran`
+-- Dumping data untuk tabel `pengeluaran`
 --
 
 INSERT INTO `pengeluaran` (`id`, `tanggal`, `keperluan`, `nama`, `jumlah`) VALUES
 (1, '2015-02-06', 'de', 'diki', 1234);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `barang_laku`
+--
+ALTER TABLE `barang_laku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_laku`
+--
+ALTER TABLE `barang_laku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
